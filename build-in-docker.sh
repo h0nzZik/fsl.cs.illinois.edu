8c51710698bdf2a1b62c81a5552d6be174bcd013
Nishant Rodrigues <nishantjr@gmail.com>
2020-12-21 00:31:55 -0600

    www: Combine {build,run}-in-docker.sh for easier maintenance

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 
 set -e
+fsl_mount=/mnt/fsl
 
-docker build -t fsl-pages .
-./run-in-docker.sh ./build
+usage() {
+    echo "TODO: Usage message"
+}
+
+fatal() {
+    echo >&2 "$@"
+    usage
+    exit 1
+}
+
+dockerrun() {
+    docker run --rm --mount src=$(cd ../; pwd),target=$fsl_mount,type=bind \
+           --workdir=$fsl_mount/www -it fsl-pages "$@"
+}
+
+command=$1
+shift || fatal "No command specified."
+
+case $command in
+    help)  usage ; exit 0 ;;
+    image) docker build -t fsl-pages . ;;
+    build) dockerrun "./build" ;;
+    bash)  dockerrun "bash" ;;
+    *)     fatal "Bad command '$command'"; exit 1;;
+esac
--- run-in-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-fsl_mount=/mnt/fsl
-
-docker run --rm --mount src=$(cd ../; pwd),target=$fsl_mount,type=bind \
-                --workdir=$fsl_mount/www -it fsl-pages "$@"
